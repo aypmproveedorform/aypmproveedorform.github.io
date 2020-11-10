a82556d1cd959e11ec8daf50b2c5439f049e0ca8
aypmproveedorform <74223718+aypmproveedorform@users.noreply.github.com>
2020-11-09 21:11:31 -0600

    Create estilos.css

---
+++ css/estilos.css
@@ -0,0 +1,25 @@
+"use strict";
+var forma = document.getElementById("forma"),
+    txtClave = forma["clave"],
+    txtNombre = forma["nombre"],
+    txtTelefono = forma["telefono"],
+    txtFecha = forma["fecha"],
+    salidaClave = document.getElementById("salidaClave"),
+    salidaNombre = document.getElementById("salidaNombre"),
+    salidaTelefono = document.getElementById("salidaTelefono"),
+    salidaFecha = document.getElementById("salidaFecha");
+
+function procesa() {
+  var clave = txtClave.value,
+      nombre = txtNombre.value,
+      telefono = txtTelefono,
+      fecha = new Date(txtFecha.value);
+      
+    
+
+  salidaClave.textContent = txtClave.value;
+  salidaNombre.textContent = txtNombre.value;
+  salidaTelefono.textContent = txtTelefono.value;  
+  salidaFecha.textContent = (fecha.getDate()+1) + "/" + (fecha.getMonth() +1) + "/" + fecha.getFullYear(); 
+  
+}
